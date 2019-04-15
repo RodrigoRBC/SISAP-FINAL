76a852baf66668bb376acac8e642785326f785d8
wyarapp <48662379+wyarapp@users.noreply.github.com>
2019-04-15 00:28:53 -0500

    index de los controladores de arbitrio
    se han modificado los index y mensajes de acciones

--- app/Controller/ArbpeopleArbratesController.php
+++ app/Controller/ArbpeopleArbratesController.php
@@ -20,9 +20,59 @@ class ArbpeopleArbratesController extends AppController {
  *
  * @return void
  */
-	public function index() {
+	// public function index() {
+	// 	$this->ArbpeopleArbrate->recursive = 0;
+	// 	$this->set('arbpeopleArbrates', $this->Paginator->paginate());
+	// }
+
+	public function index($paginador=null) {
+
 		$this->ArbpeopleArbrate->recursive = 0;
-		$this->set('arbpeopleArbrates', $this->Paginator->paginate());
+		$this->set('arbpeoplepublics', $this->Paginator->paginate());
+
+		if(!empty($this->request->params['named']['page'])){
+			$this->Session->write('Arbpeoplepublic.page',$this->request->params['named']['page']);
+			$this->request->params['named']['page'] = $this->Session->read('Arbpeoplepublic.page');
+		}
+
+		$this->set('paginador',$paginador);
+
+		$elementos = array(
+			'Arbpeoplepublic.firstname'=>__('NOMBRES', true),
+			'Arbpeoplepublic.appaterno'=>__('APELLIDO PATERNO', true),
+			'Arbpeoplepublic.apmaterno'=>__('APELLIDO MATERNO', true)
+		);
+
+		$this->set('elementos',$elementos);
+		if(!empty($this->params['named']['valor']) || !empty($this->params['named']['desactivo']))
+		{
+			$this->request->data['Buscar']['buscador'] = $this->params['named']['buscador'];
+			$this->request->data['Buscar']['valor'] = $this->params['named']['valor'];
+			$this->request->data['Buscar']['desactivo'] = $this->params['named']['desactivo'];
+		}
+		if(empty($this->request->data['Buscar']['buscador']))
+			$this->request->data['Buscar']['valor'] = null;
+
+		$valorDeBusqueda = isset($this->request->data['Buscar']['valor'])?trim($this->request->data['Buscar']['valor']):null;
+		$conditions = !empty($valorDeBusqueda)?
+						array($this->request->data['Buscar']['buscador'].' LIKE'=>'%'.trim($this->request->data['Buscar']['valor']).'%'):
+						array();
+
+		$conditionsActivos = (!empty($this->request->data['Buscar']['desactivo']) == 1) ?
+								array('ArbpeopleArbrate.status'=>'DE') :
+								array('ArbpeopleArbrate.status'=>'AC');
+
+		$conditions = $conditions + $conditionsActivos;
+
+		$this->paginate = array('limit' => 10,
+								'page' => 1,
+								'order' => array ('ArbpeopleArbrate.created' => 'asc'),
+								'conditions' => $conditions,
+								'recursive'=>1
+								);
+
+		$arbpeopleArbrates=$this->paginate('ArbpeopleArbrate');
+		$this->set(compact('arbpeopleArbrates'));
 	}
 
 /**
@@ -34,7 +84,7 @@ class ArbpeopleArbratesController extends AppController {
  */
 	public function view($id = null) {
 		if (!$this->ArbpeopleArbrate->exists($id)) {
-			throw new NotFoundException(__('Invalid arbpeople arbrate'));
+			throw new NotFoundException(__('Pago de la persona inválido'));
 		}
 		$options = array('conditions' => array('ArbpeopleArbrate.' . $this->ArbpeopleArbrate->primaryKey => $id));
 		$this->set('arbpeopleArbrate', $this->ArbpeopleArbrate->find('first', $options));
@@ -49,10 +99,10 @@ class ArbpeopleArbratesController extends AppController {
 		if ($this->request->is('post')) {
 			$this->ArbpeopleArbrate->create();
 			if ($this->ArbpeopleArbrate->save($this->request->data)) {
-				$this->Flash->success(__('The arbpeople arbrate has been saved.'));
+				$this->Flash->success(__('El pago de la tasa de arbitrio de la persona ha sido guardado.'));
 				return $this->redirect(array('action' => 'index'));
 			} else {
-				$this->Flash->error(__('The arbpeople arbrate could not be saved. Please, try again.'));
+				$this->Flash->error(__('El pago de la tasa de arbitrio de la persona no pudo ser guardado. Inténtalo de nuevo.'));
 			}
 		}
 		$arbpeople = $this->ArbpeopleArbrate->Arbperson->find('list');
@@ -69,14 +119,14 @@ class ArbpeopleArbratesController extends AppController {
  */
 	public function edit($id = null) {
 		if (!$this->ArbpeopleArbrate->exists($id)) {
-			throw new NotFoundException(__('Invalid arbpeople arbrate'));
+			throw new NotFoundException(__('Pago de la persona inválido'));
 		}
 		if ($this->request->is(array('post', 'put'))) {
 			if ($this->ArbpeopleArbrate->save($this->request->data)) {
-				$this->Flash->success(__('The arbpeople arbrate has been saved.'));
+				$this->Flash->success(__('El pago de la tasa de arbitrio de la persona ha sido modificado.'));
 				return $this->redirect(array('action' => 'index'));
 			} else {
-				$this->Flash->error(__('The arbpeople arbrate could not be saved. Please, try again.'));
+				$this->Flash->error(__('El pago de la tasa de arbitrio de la persona no pudo ser modificado. Inténtalo de nuevo.'));
 			}
 		} else {
 			$options = array('conditions' => array('ArbpeopleArbrate.' . $this->ArbpeopleArbrate->primaryKey => $id));
@@ -97,13 +147,13 @@ class ArbpeopleArbratesController extends AppController {
 	public function delete($id = null) {
 		$this->ArbpeopleArbrate->id = $id;
 		if (!$this->ArbpeopleArbrate->exists()) {
-			throw new NotFoundException(__('Invalid arbpeople arbrate'));
+			throw new NotFoundException(__('Pago de la persona inválido'));
 		}
 		$this->request->allowMethod('post', 'delete');
 		if ($this->ArbpeopleArbrate->delete()) {
-			$this->Flash->success(__('The arbpeople arbrate has been deleted.'));
+			$this->Flash->success(__('El pago de la tasa de arbitrio de la persona ha sido eliminado.'));
 		} else {
-			$this->Flash->error(__('The arbpeople arbrate could not be deleted. Please, try again.'));
+			$this->Flash->error(__('El pago de la tasa de arbitrio de la persona no pudo ser eliminado. Inténtalo de nuevo.'));
 		}
 		return $this->redirect(array('action' => 'index'));
 	}
--- app/Controller/ArbubigeosController.php
+++ app/Controller/ArbubigeosController.php
@@ -20,11 +20,64 @@ class ArbubigeosController extends AppController {
  *
  * @return void
  */
-	public function index() {
-		$this->Arbubigeo->recursive = 0;
-		$this->set('arbubigeos', $this->Paginator->paginate());
-	}
+	// public function index() {
+	// 	$this->Arbubigeo->recursive = 0;
+	// 	$this->set('arbubigeos', $this->Paginator->paginate());
+	// }
+/**
+ * index method
+ *
+ * @return void
+ */
+	public function index($paginador=null) {
 
+		$this->Arbubigeo->recursive = 0;
+		$this->set('arbpeoplepublics', $this->Paginator->paginate());
+
+		if(!empty($this->request->params['named']['page'])){
+			$this->Session->write('Arbpeoplepublic.page',$this->request->params['named']['page']);
+			$this->request->params['named']['page'] = $this->Session->read('Arbpeoplepublic.page');
+		}
+
+		$this->set('paginador',$paginador);
+
+		$elementos = array(
+			'Arbpeoplepublic.firstname'=>__('NOMBRES', true),
+			'Arbpeoplepublic.appaterno'=>__('APELLIDO PATERNO', true),
+			'Arbpeoplepublic.apmaterno'=>__('APELLIDO MATERNO', true)
+		);
+
+		$this->set('elementos',$elementos);
+		if(!empty($this->params['named']['valor']) || !empty($this->params['named']['desactivo']))
+		{
+			$this->request->data['Buscar']['buscador'] = $this->params['named']['buscador'];
+			$this->request->data['Buscar']['valor'] = $this->params['named']['valor'];
+			$this->request->data['Buscar']['desactivo'] = $this->params['named']['desactivo'];
+		}
+		if(empty($this->request->data['Buscar']['buscador']))
+			$this->request->data['Buscar']['valor'] = null;
+
+		$valorDeBusqueda = isset($this->request->data['Buscar']['valor'])?trim($this->request->data['Buscar']['valor']):null;
+		$conditions = !empty($valorDeBusqueda)?
+						array($this->request->data['Buscar']['buscador'].' LIKE'=>'%'.trim($this->request->data['Buscar']['valor']).'%'):
+						array();
+
+		$conditionsActivos = (!empty($this->request->data['Buscar']['desactivo']) == 1) ?
+								array('Arbubigeo.status'=>'DE') :
+								array('Arbubigeo.status'=>'AC');
+
+		$conditions = $conditions + $conditionsActivos;
+
+		$this->paginate = array('limit' => 10,
+								'page' => 1,
+								'order' => array ('Arbubigeo.created' => 'asc'),
+								'conditions' => $conditions,
+								'recursive'=>1
+								);
+
+		$arbubigeos=$this->paginate('Arbubigeo');
+		$this->set(compact('arbubigeos'));
+	}
 /**
  * view method
  *
@@ -34,7 +87,7 @@ class ArbubigeosController extends AppController {
  */
 	public function view($id = null) {
 		if (!$this->Arbubigeo->exists($id)) {
-			throw new NotFoundException(__('Invalid arbubigeo'));
+			throw new NotFoundException(__('Ubigeo inválido'));
 		}
 		$options = array('conditions' => array('Arbubigeo.' . $this->Arbubigeo->primaryKey => $id));
 		$this->set('arbubigeo', $this->Arbubigeo->find('first', $options));
@@ -49,10 +102,10 @@ class ArbubigeosController extends AppController {
 		if ($this->request->is('post')) {
 			$this->Arbubigeo->create();
 			if ($this->Arbubigeo->save($this->request->data)) {
-				$this->Flash->success(__('The arbubigeo has been saved.'));
+				$this->Flash->success(__('El ubigeo ha sido guardado.'));
 				return $this->redirect(array('action' => 'index'));
 			} else {
-				$this->Flash->error(__('The arbubigeo could not be saved. Please, try again.'));
+				$this->Flash->error(__('El ubigeo no pudo ser guardado. Inténtalo de nuevo.'));
 			}
 		}
 	}
@@ -66,14 +119,14 @@ class ArbubigeosController extends AppController {
  */
 	public function edit($id = null) {
 		if (!$this->Arbubigeo->exists($id)) {
-			throw new NotFoundException(__('Invalid arbubigeo'));
+			throw new NotFoundException(__('Ubigeo inválido'));
 		}
 		if ($this->request->is(array('post', 'put'))) {
 			if ($this->Arbubigeo->save($this->request->data)) {
-				$this->Flash->success(__('The arbubigeo has been saved.'));
+				$this->Flash->success(__('El ubigeo ha sido modificado.'));
 				return $this->redirect(array('action' => 'index'));
 			} else {
-				$this->Flash->error(__('The arbubigeo could not be saved. Please, try again.'));
+				$this->Flash->error(__('El ubigeo no pudo ser modificado. Inténtalo de nuevo.'));
 			}
 		} else {
 			$options = array('conditions' => array('Arbubigeo.' . $this->Arbubigeo->primaryKey => $id));
@@ -91,13 +144,13 @@ class ArbubigeosController extends AppController {
 	public function delete($id = null) {
 		$this->Arbubigeo->id = $id;
 		if (!$this->Arbubigeo->exists()) {
-			throw new NotFoundException(__('Invalid arbubigeo'));
+			throw new NotFoundException(__('Ubigeo inválido'));
 		}
 		$this->request->allowMethod('post', 'delete');
 		if ($this->Arbubigeo->delete()) {
-			$this->Flash->success(__('The arbubigeo has been deleted.'));
+			$this->Flash->success(__('El ubigeo ha sido eliminado.'));
 		} else {
-			$this->Flash->error(__('The arbubigeo could not be deleted. Please, try again.'));
+			$this->Flash->error(__('El ubigeo no pudo ser eliminado. Inténtalo de nuevo.'));
 		}
 		return $this->redirect(array('action' => 'index'));
 	}
